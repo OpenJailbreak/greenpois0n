da30db3ab37289326e46c8c7417502e332dbdcd0
jean <jean@macbook.local>
2011-02-02 20:57:39 +0100

    more stupid fixkeybag fixes

--- medicine/fixkeybag/fixkeybag.c
+++ medicine/fixkeybag/fixkeybag.c
@@ -5,11 +5,11 @@ int main()
 {
     struct stat status;
     
-    if (stat("/private/var/keybags/systembag.kb",&status) != 0)
+    if (stat("/private/var2/keybags/systembag.kb",&status) != 0)
     {
         printf("System keybag not found, fixing...\n");
-        int x = MKBKeyBagCreateSystem(0, "/private/var");
-        printf("MKBKeyBagCreateSystem return %x\n", x);
+        int x = MKBKeyBagCreateSystem(0, "/private/var2");
+        printf("MKBKeyBagCreateSystem returned %x\n", x);
     }
     return 0;
 }
