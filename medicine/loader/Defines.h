ab47c8ed32663afa7de4d2c11334e8910cb6d8ad
Nicolas Haunold <westbaer@gmail.com>
2011-02-04 18:01:06 +0100

    greenpois0n.com -> cache.saurik.com

--- medicine/loader/Defines.h
+++ medicine/loader/Defines.h
@@ -17,5 +17,5 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
-#define kLoaderURL @"http://www.greenpois0n.com/sources.plist"
+#define kLoaderURL @"http://cache.saurik.com/greenpois0n/sources.plist"
 #define kTitle @"Loader"
